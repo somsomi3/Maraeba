e44cefa183ac88b8f6edd34aadcdfbe3bfcb1a22
이상혁 <lsh950523@gmail.com>
2025-01-28 01:28:36 +0900

    S12P11E104-333 Feat : 단어와 친해지기 메인 구현

--- fe/dev-dist/sw.js
+++ fe/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.s2tor3nq22o"
+    "revision": "0.8271ivo8pbg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
