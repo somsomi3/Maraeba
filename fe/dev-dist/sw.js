0cee42d2ebba93ee8bf4623d27d93c52c937361b
이상혁 <lsh950523@gmail.com>
2025-01-27 20:18:37 +0900

    Save local changes before pulling

--- fe/dev-dist/sw.js
+++ fe/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.esfn71nnd7g"
+    "revision": "0.20g1dvuegug"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
