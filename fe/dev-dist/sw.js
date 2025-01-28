246290d128132914862bc0e3e44768912fb4f34d
박수민 <qkrtnals032@gmail.com>
2025-01-28 16:18:18 +0900

    S12P11E104-361 Design: 로고 크기 변경, 발음 메인 페이 지 위치 조정

--- fe/dev-dist/sw.js
+++ fe/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.cuqpmhc8je"
+    "revision": "0.52nrtt8m9ro"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
