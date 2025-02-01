b7162897934c89801cd77e8d4ee1a2b281f79947
김정우 <3957ki@gmail.com>
2025-02-01 15:58:22 +0900

    S12P11E104-444 Feat: 발음 관련 Swagger 추가

--- be/src/main/java/com/be/domain/prons/controller/PronsController.java
+++ be/src/main/java/com/be/domain/prons/controller/PronsController.java
@@ -1,5 +1,18 @@
 package com.be.domain.prons.controller;
 
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.be.common.exception.CustomException;
 import com.be.common.exception.ErrorCode;
 import com.be.common.model.response.BaseResponseBody;
@@ -14,22 +27,14 @@ import com.be.domain.prons.response.GetSessionRes;
 import com.be.domain.prons.response.GetSpecificDataRes;
 import com.be.domain.prons.response.PostSessionRes;
 import com.be.domain.prons.service.PronsService;
-import jakarta.validation.constraints.NotBlank;
-import java.util.List;
-import java.util.UUID;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "PronsController", description = "발음 수업 관련 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/prons")
@@ -38,6 +43,7 @@ public class PronsController {
 	private final PronsService pronsService;
 
 	// 발음 수업 카테고리 가져오기
+	@Operation(summary = "발음 수업 카테고리 가져오기", description = "사용자가 접근할 수 있는 발음 수업 카테고리를 반환합니다.")
 	@GetMapping
 	public GetClassesRes getClasses() {
 		List<PronunciationClassDTO> classDTOS = pronsService.getClasses();
@@ -45,6 +51,7 @@ public class PronsController {
 	}
 
 	// 특정 수업 학습자료 가져오기
+	@Operation(summary = "특정 수업 학습자료 가져오기", description = "특정 발음 수업의 학습 자료를 반환합니다.")
 	@GetMapping("/class/{class_id}")
 	public GetClassDataRes getClassData(@NotNull @PathVariable("class_id") Long classId) {
 		List<PronunciationDataDTO> dataDTOS = pronsService.getClassData(classId);
@@ -52,6 +59,7 @@ public class PronsController {
 	}
 
 	// 특정 수업 특정 발음 자료 가져오기
+	@Operation(summary = "특정 수업 특정 발음 학습자료 가져오기", description = "특정 수업 특정 발음의 학습 자료를 반환합니다.")
 	@GetMapping("/class/{class_id}/seq/{seq_id}")
 	public GetSpecificDataRes getSpecificData(@NotNull @PathVariable("class_id") Long classId,
 		@NotNull @PathVariable("seq_id") Integer sequence) {
@@ -60,6 +68,7 @@ public class PronsController {
 	}
 
 	// 수업 세션 생성 (사용자가 수업 시작)
+	@Operation(summary = "수업 세션 생성", description = "수업 세션을 시작합니다.")
 	@PostMapping("/start")
 	public PostSessionRes startSession(@Validated @RequestBody PostSessionReq request) {
 		String id = UUID.randomUUID().toString(); // 세션 ID 생성
@@ -69,16 +78,18 @@ public class PronsController {
 	}
 
 	// 세션 조회
+	@Operation(summary = "특정 세션 조회", description = "특정 세션의 정보를 가져옵니다.")
 	@GetMapping("/session/{session_id}")
 	public GetSessionRes getLessonSession(@NotBlank @PathVariable("session_id") String id) {
 		PronunciationSessionDTO sessionDTO = pronsService.getSession(id);
-		if(sessionDTO == null) {
+		if (sessionDTO == null) {
 			throw new CustomException(ErrorCode.SESSION_NOT_FOUND);
 		}
 		return new GetSessionRes("Success", HttpStatus.OK, sessionDTO);
 	}
 
 	// 세션 삭제 (수업 종료)
+	@Operation(summary = "세션 종료", description = "수업 세션을 종료합니다.")
 	@DeleteMapping("/session/{session_id}")
 	public BaseResponseBody endLesson(@NotBlank @PathVariable("session_id") String id) {
 		pronsService.deleteSession(id);
@@ -86,6 +97,7 @@ public class PronsController {
 	}
 
 	// 발음 유사도 저장
+	@Operation(summary = "발음 유사도 저장", description = "발음 유사도를 저장합니다.")
 	@PostMapping("/session/similarity")
 	public BaseResponseBody savePronunciationSimilarity(@Validated @RequestBody PostSimilarityReq request) {
 		pronsService.savePronunciationSimilarity(request.getSessionId(), request.getSimilarity());
@@ -93,6 +105,7 @@ public class PronsController {
 	}
 
 	// 히스토리 및 통계 저장
+	@Operation(summary = "히스토리 및 통계 저장", description = "수업을 끝내기 전 히스토리와 통계를 저장합니다.")
 	@PostMapping("/session/history/{session_id}")
 	public BaseResponseBody saveSessionHistory(@NotBlank @PathVariable("session_id") String id) {
 		pronsService.saveSessionHistory(id);
