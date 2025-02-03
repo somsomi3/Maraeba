de9d7d0d8634def50158d6cf27a5482df09d12e4
이은선 <98silverline@gmail.com>
2025-02-03 11:42:25 +0900

    Feat: 단어게임 API 일부 수정

--- be/src/main/java/com/be/BeApplication.java
+++ be/src/main/java/com/be/BeApplication.java
@@ -7,25 +7,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication
 public class BeApplication {
     public static void main(String[] args) {
         SpringApplication.run(BeApplication.class, args);
-    }
-
-
-    //Cross-Origin Resource Sharing (CORS)
-    //http://localhost:5173/ 으로부터 오는 모든 요청 허용하기
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedMethods("*")
-                        .allowedOrigins("http://localhost:5173"); //http://localhost:5173에서만 오는 요청 허용
-            }
-        };
     }
 }
--- fe/dev-dist/sw.js
+++ fe/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.l477lp07dq"
+    "revision": "0.dki42jtn4sg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
