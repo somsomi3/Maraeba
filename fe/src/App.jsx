58f3c6030261a91294107d7cdad482de72e1ce58
박수민 <qkrtnals032@gmail.com>
2025-02-05 16:52:43 +0900

    Fix: CSS 수정

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -1,9 +1,9 @@
-import './App.css';
+import './index.css'; // ✅ CSS 경로 변경
 import { Login } from './features/auth';
 
 function App() {
   return (
-    <div> 
+    <div className="app-container"> {/* ✅ 중앙 정렬 추가 */}
       <Login />
     </div>
   );
