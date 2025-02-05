5755fe809b257be77bb830b8407e213350513577
박수민 <qkrtnals032@gmail.com>
2025-02-05 17:09:43 +0900

    S12P11E104-490 Fix: 결과 페이지 css 수정 및 전체 css 약간 수정

--- fe/dev-dist/sw.js
+++ fe/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-9dc17825'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.2gbtfkmg8so"
+    "revision": "0.9bbh19f9kn"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -1,9 +1,9 @@
-import './index.css'; // ✅ CSS 경로 변경
+import './App.css';
 import { Login } from './features/auth';
 
 function App() {
   return (
-    <div className="app-container"> {/* ✅ 중앙 정렬 추가 */}
+    <div className="app-container"> 
       <Login />
     </div>
   );
