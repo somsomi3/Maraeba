29e03f274bc739c97f89b70b23af66274a1961f6
박수민 <qkrtnals032@gmail.com>
2025-02-05 17:52:08 +0900

    S12P11E104-463 Feat: 카카오 로그인 관련 수정

--- fe/src/features/auth/index.js
+++ fe/src/features/auth/index.js
@@ -2,3 +2,5 @@ export { default as FindId } from './FindId';
 export { default as FindPw } from './FindPw';
 export { default as Login } from './Login';
 export { default as Register } from './Register';
+export { default as KakaoCallback } from './KakaoCallback';
+export { default as KakaoLogin } from './KakaoLogin';
