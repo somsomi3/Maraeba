35a7f412da0e13df862d595802cf60627904c01c
정한균 <j1113019@naver.com>
2025-02-06 09:19:29 +0900

    S12P11E104-458 Chore: main.js에서 </React.StrictMode> 주석 처리
    - 개발 중 Strict Mode로 인한 이중 렌더링 이슈 임시 해결을 위해 </React.StrictMode> 부분을 주석 처리함

--- fe/src/main.jsx
+++ fe/src/main.jsx
@@ -96,9 +96,9 @@ const router = createBrowserRouter(
 
 // ✅ Redux Provider로 전체 앱 감싸기
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <RouterProvider router={router} />
-        </Provider>
-    </React.StrictMode>
+    //<React.StrictMode>
+    <Provider store={store}>
+        <RouterProvider router={router} />
+    </Provider>
+    //</React.StrictMode>
 );
