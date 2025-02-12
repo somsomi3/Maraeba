227e794de3375f3f011b47941ed0914769c37dc8
이은선 <98silverline@gmail.com>
2025-02-12 10:15:02 +0900

    Feat: 일부 log삭제

--- be/src/main/java/com/be/domain/wgames/findAnimals/controller/FindGameController.java
+++ be/src/main/java/com/be/domain/wgames/findAnimals/controller/FindGameController.java
@@ -20,15 +20,12 @@ public class FindGameController {
     @GetMapping("/start-game")
     public ResponseEntity<AnimalResponse> startGame() throws IOException {
         AnimalResponse animalResponse = findGameService.pickAnimal();
-        System.out.println("맞춰야 할 이미지 번호: " + animalResponse.getImageNumber());
         return ResponseEntity.ok(animalResponse);
     }
 
     @PostMapping("/is-correct")
     public ResponseEntity<AnimalAnswerResponse> isCorrect(@ModelAttribute AnimalCorrectRequest request) throws IOException {
-        System.out.println("여기에 옴!");
         AnimalAnswerResponse answerResponse = findGameService.isCorrect(request);
-        System.out.println(answerResponse.getAnimalName());
         return ResponseEntity.ok(answerResponse);
     }
 }
