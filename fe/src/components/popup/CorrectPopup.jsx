38662ebc6a143f39f0add28bedfe1491f8d1c10a
이상혁 <lsh950523@naver.com>
2025-02-12 13:55:54 +0900

    S12P11E104-543 Fix : 단어게임, 숨은 그림찾기 버그 수정

---
+++ fe/src/components/popup/CorrectPopup.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./CorrectPopup.css";
+
+const CorrectPopup = ({ message, onRestart }) => {
+  return (
+    <div className="popup-overlay">
+      <div className="popup-content">
+        <p>{message}</p>
+        <button className="popup-button" onClick={onRestart}>게임 시작</button>
+      </div>
+    </div>
+  );
+};
+
+export default CorrectPopup;
